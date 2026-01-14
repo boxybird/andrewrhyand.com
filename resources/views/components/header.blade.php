01143016dd935d13eb719522558f42e9fc870ec7
boxybird <andrew.rhyand@gmail.com>
2026-01-14 14:02:07 -0700

    Make header link text responsive based on screen size

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -2,7 +2,8 @@
     <nav class="gap-4 flex font-bold justify-between px-6 py-4">
         <div class="flex gap-2 items-center">
             <span class="bg-amber-400 h-4 rounded-full w-4"></span>
-            <a class="hover:text-red-900" href="{{ route('home') }}">andrewrhyand.com</a>
+            <a class="sm:hidden hover:text-red-900" href="{{ route('home') }}">andrew</a>
+            <a class="hidden sm:inline hover:text-red-900" href="{{ route('home') }}">andrew rhyand</a>
         </div>
         <div class="flex gap-4 text-red-700">
             <a class="hover:text-red-900" href="{{ route('posts.index') }}">posts</a>
